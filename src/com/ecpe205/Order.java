001c773fe10b12129911d293b99f2c437490bcb7
Raymund De La Victoria <raymunddlv@gmail.com>
2022-05-13 11:51:37 +0800

    modified Order.java

--- src/com/ecpe205/Order.java
+++ src/com/ecpe205/Order.java
@@ -31,30 +31,28 @@ public class Order {
      * @param index
      */
     public void removeItem(int index){
-        //edit starts here
-        items.remove(index);
-        //edit ends here
+        this.items.remove(index);
     }
-
-
     /**
      * Return the total quantity within an order
      * @return Total Quantity
      */
     public double getTotalQuantity() {
-        //edit starts here
-        return getTotalQuantity(); //please remove
-        //edit ends here
+        double total=0;
+        for (int i=0; i<this.items.size(); i++){
+            total += items.get(i).getQuantity();
+        }
+        return total;
     }
-
     /**
      * Returns the overall total of all the items
      * @return Overall Total
      */
     public double getGrandTotal() {
-        //edit starts here
-        double amount = items.size();
-        return amount; //please remove
-        //edit ends here
+        double grandTotal=0;
+        for (int i=0; i<items.size();i++){
+            grandTotal += items.get(i).getAmount();
+        }
+        return grandTotal;
     }
 }
