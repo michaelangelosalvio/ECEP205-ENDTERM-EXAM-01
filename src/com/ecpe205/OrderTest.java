6737bba95f659fc8bde8b6c64ba031883c797bb2
Chaaang <johnmarchangggggg@gmail.com>
2022-05-13 11:56:08 +0800

    3rd commit

--- src/com/ecpe205/OrderTest.java
+++ src/com/ecpe205/OrderTest.java
@@ -12,6 +12,10 @@ class OrderTest {
     @BeforeAll
     void setup(){
         ord = new Order();
+        ord.addItem(new Product("123", "saging", 5.0), 5);
+        ord.addItem(new Product("234", "talong", 5.0), 5);
+        ord.addItem(new Product("345", "watermelon", 5.0), 5);
+        ord.addItem(new Product("678", "berry", 5.0), 5);
     }
 
     @ParameterizedTest
@@ -34,6 +38,20 @@ class OrderTest {
         ord.addItem(new Product("222", "banana", 5.0), 5);
         ArrayList<Item> items = ord.getItems();
 
+
+    }
+    @ParameterizedTest
+    void ShouldGetTotalQuantity(){
+        assertEquals(5, ord.getTotalQuantity());
+
+    }
+    @ParameterizedTest
+    void ShouldRemoveItem(){
+        for(int i = 0; i< ord.getItems().size(); i++){
+            ord.removeItem(i);
+        }
+
+        assertEquals(5, ord.getTotalQuantity());
 
     }
 }
