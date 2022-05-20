984160e9dfa1aa91f357ee5c6f6b6afd46b4d680
Sharmaine Nicole Ditchon <s2021406@usls.edu.ph>
2022-05-20 11:12:49 +0800

    Item test added

--- src/com/ecpe205/ItemUnitTest.java
+++ src/com/ecpe205/ItemUnitTest.java
@@ -1,11 +1,32 @@
 package com.ecpe205;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
-class ItemUnitTest {
 
-    public static void main(String[] args) {
-        
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ItemTest {
+    @Test
+    void getAmount(){
+
+        // 6-1.
+        Item item = new Item(new Product("11111","wagyu beef",2500),2);
+        assertEquals(5000, item.getAmount());
+
+        //6-2.
+        Item item2 = new Item(new Product("22222","spaghetti",100),10);
+        assertEquals(1000, item2.getAmount());
+        item2.setProduct(new Product("22222", "pancit", 40));
+        assertEquals(400, item2.getAmount());
+
+        //6-3.
+        Item item3 = new Item(new Product("33333","lumpia",25),2);
+        assertEquals(2,item3.getQuantity());
+        item3.setQuantity(50);
+        assertEquals(50, item3.getQuantity());
     }
-
 }
